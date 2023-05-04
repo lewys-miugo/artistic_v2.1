b16386f9aecf35759f7c65060809e9fa53bc04eb
lewys-miugo <lewiswambugu01@gmail.com>
2023-05-04 19:24:24 +0300

    stunning wall art

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,60 @@
 @extends('layouts.nav_footer')
 
 @section('content')
+<div class="mx-4">
+    <!-- 3-images -->
+    <div class="flex flex-row mt-2">
+        <div class="basis-2/4">
+            <img src="https://cdn.shopify.com/s/files/1/1568/8443/files/wall-art-0124.png?v=1679570066&width=720" alt="">
+        </div>
+        <div class="basis-1/4 mx-2">
+            <img src="https://cdn.shopify.com/s/files/1/1568/8443/files/wall-decor-124.png?v=1679569588&width=360" alt="">
+        </div>
+        <div class="basis-1/4">
+           <img src="https://cdn.shopify.com/s/files/1/1568/8443/files/wall-art-decorations-124.png?v=1679569588&width=360" alt="">
+        </div>
+    </div>
+
+    <!-- stunning wall art -->
+    <div class="mx-32 mt-20">
+        
+        <h1 class="text-4xl flex justify-center mb-8 ">Stunning Wall Art at an Affordable Price</h1>
+        <h5 class="text-lg px-36 flex justify-center text-center">We’ve curated an accessible, extensive selection of wall art. Discover top-trending designs and create your dream interior in the blink of an eye!</h5>
+        
+    </div>
+
+    <div class="mx-20 my-8">
+        <ul class="flex flex-row justify-center">
+            <li class="uppercase flex flex-row mx-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                </svg>
+                <p class=""> 100% High Quality </p>
+            </li>
+            <li class="uppercase flex flex-row mx-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5" />
+                </svg>
+
+                <p class=""> made in kenya </p>
+            </li>
+            <li class="uppercase flex flex-row mx-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                </svg>
+
+                <p class=""> over 20k 5 star reviews </p>
+            </li>
+            <li class="uppercase flex flex-row mx-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                </svg>
+
+                <p class=""> 30 day free returns</p>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
 @endsection
