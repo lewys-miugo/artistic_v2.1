e5b9f741337cf8a6a97da48707906840cdbe1432
lewys-miugo <lewiswambugu01@gmail.com>
2023-05-25 08:57:45 +0300

    Making dropdown

--- resources/views/layouts/nav_footer.blade.php
+++ resources/views/layouts/nav_footer.blade.php
@@ -26,15 +26,44 @@
       <li class="font-semibold text-gray-700">Free Shipping</li>
       <li class="font-semibold text-gray-700">Returns</li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-        </svg>
+        <button class="group">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            	<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        	</svg>
+			<div class="absolute hidden group-focus:block -ml-28 bg-white shadow-md mt-1 rounded">
+				
+				<ul class="text-center border rounded">
+					<li class="px-4 py-1  m-4">Create your account to:</li>
+					<li class="px-12 border-t "></li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b text-sm">Save your wishlist</li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b text-sm">Track your order</li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b text-sm">Save your order data</li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b text-sm">Register</li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b text-sm">already have an account? <span><a href="#">login</a></span> </li>
+
+				</ul>
+				
+			</div>
+
+		</button>
 
       </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-        </svg>
+      <li >
+        <button class="group">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            	<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+        	</svg>
+
+			<div class="absolute hidden group-focus:block min-w-full w-max bg-white shadow-md mt-1 rounded">
+				<ul class="text-left border rounded">
+					<li class="px-4 py-1 hover:bg-gray-100 border-b">Menu list 1</li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b">Menu list 1</li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b">Menu list 1</li>
+					<li class="px-4 py-1 hover:bg-gray-100 border-b">Menu list 1</li>
+					<li class="px-4 py-1 hover:bg-gray-100 ">Menu list 1</li>
+				</ul>
+			</div>
+		</button>
 
       </li>
       <li>
