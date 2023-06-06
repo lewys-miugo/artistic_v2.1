26f1b2bf0eb746ad22971d2829488d1b30d80c46
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-06 06:48:21 +0300

    Adding shop details page

---
+++ resources/views/livewire/details-component.blade.php
@@ -0,0 +1,66 @@
+<div>
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+
+
+    <body class="bg-gray-100">
+        <div class="container mx-auto p-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div class="p-4">
+                    <img src="{{asset('images/product--1.webp')}}" alt="Art Product" class="w-full">
+                </div>
+                <div class="p-4">
+                    <h1 class="text-3xl font-bold mb-2">Art Product Title</h1>
+                    <p class="text-gray-700 text-lg mb-4">Artist Name</p>
+                    <p class="text-gray-600 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam congue
+                        turpis quis ligula finibus tincidunt. Fusce lobortis pharetra libero, id commodo metus.</p>
+                    <div class="flex items-center mb-6">
+                        <span class="mr-2">Rating:</span>
+                        <div class="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                class="h-4 w-4 text-yellow-400">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 13l4 4L19 7"></path>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                class="h-4 w-4 text-yellow-400">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 13l4 4L19 7"></path>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                class="h-4 w-4 text-yellow-400">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 13l4 4L19 7"></path>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                class="h-4 w-4 text-gray-400">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 13l4 4L19 7"></path>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                class="h-4 w-4 text-gray-400">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <p class="text-gray-700 text-xl mb-4">$99.99</p>
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Add to Cart
+                    </button>
+                </div>
+            </div>
+            <div class="mt-8">
+                <h2 class="text-2xl font-bold mb-4">Product Details</h2>
+                <ul class="list-disc list-inside">
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    <li>Nullam congue turpis quis ligula finibus tincidunt.</li>
+                    <li>Fusce lobortis pharetra libero, id commodo metus.</li>
+                    <li>Suspendisse eu sem in tellus venenatis rhoncus.</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+
+
+</div>
