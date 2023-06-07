15034c3d0106687d00a076a3e53885f29a77e254
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-08 02:46:27 +0300

    Adding functionality to Clear Cart container

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -76,7 +76,7 @@
 
                 
                 <div class="flex items-right">
-                    <button class="text-red-600 hover:text-red-700 focus:outline-none">
+                    <button class="text-red-600 hover:text-red-700 focus:outline-none" wire:click.prevent="clearAll()">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                         </svg> Clear cart
