f75816ec7637d1efb577327feb86aba4ff16f3df
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-08 02:44:00 +0300

    Making cart icon dynamic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -147,9 +147,13 @@
 
                         </li>
                         <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                            </svg>
+                            </svg> -->
+
+
+                            <!-- Cart component -->
+                            @livewire('cart-icon-component')
 
                         </li>
                         </ul>
@@ -333,6 +337,8 @@
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
         @livewireScripts
+       
+        <!-- Making drop down menu on user -->
         <script>
             window.addEventListener('DOMContentLoaded', ()=>{
                 const usrBtn=document.querySelector('#usr-btn')
@@ -354,5 +360,48 @@
                 })
             });
         </script>
+
+        <!-- making nav-link 2 on shop page functional -->
+        <!-- <script>
+            const navLinks = document.querySelectorAll('.nav-link');
+
+            navLinks.forEach(link => {
+                link.addEventListener('click', function(event) {
+                    event.preventDefault();
+
+                    if (!this.classList.contains('border-b-2')) {
+                            Remove the active class from all links
+                            navLinks.forEach(link => {
+                            link.classList.remove('border-b-2', 'border-gray-800');
+                            });
+
+                    Add the active class to the clicked link
+                    this.classList.add('border-b-2', 'border-gray-800');
+                    }
+                });
+            });
+        </script> -->
+
+        <script>
+            const navLinks = document.querySelectorAll('.nav-link');
+
+            navLinks.forEach(link => {
+                link.addEventListener('click', function(event) {
+                    event.preventDefault();
+
+                    if (!this.classList.contains('border-b-2')) {
+                            // Remove the active class from all links
+                            navLinks.forEach(link => {
+                            link.classList.remove('border-b-2', 'border-gray-800');
+                            });
+
+                    // Add the active class to the clicked link
+                    this.classList.add('border-b-2', 'border-gray-800');
+                    }
+                });
+            });
+        </script>
+
+
     </body>
 </html>
