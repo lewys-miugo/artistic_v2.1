6264c8e945c66a0a8988e4626c96ffb5afe7aaa7
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-07 06:31:44 +0300

    Adding increment, decrement and delete functinality in the cart component

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,30 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
+    }
+
+     public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function destroy($id)
+    {
+        Cart::remove($id);
+        session()->flash('success_message','Item has been removed!');
+    }
+
     public function render()
     {
         return view('livewire.cart-component');
