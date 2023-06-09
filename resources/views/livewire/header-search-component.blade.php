7c2524ee312898941efd7c6ee697bc99d842b1ea
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-09 12:25:12 +0300

    Making the search component,value, read what is beig searched for

--- resources/views/livewire/header-search-component.blade.php
+++ resources/views/livewire/header-search-component.blade.php
@@ -1,6 +1,6 @@
 <div class="relative">
     <form action="{{route('product.search')}}" >
-        <input id="searchInput" type="text" name="q" class="w-full rounded-md pl-2 pr-10 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Search">
+        <input id="searchInput" type="text" name="q" class="w-full rounded-md pl-2 pr-10 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Search" value="{{$q}}">
 
         <div id="searchIcon" class="absolute inset-y-0 right-0 flex items-center pr-3">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5 cursor-pointer">
