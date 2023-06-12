cc8b8c6a6f768b8f88edfdd5c059a6a89747038a
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-12 15:45:13 +0300

    Adding the admin/addProduct route.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
-
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
     
 
@@ -61,6 +61,7 @@ Route::middleware(['auth','authadmin'])->group(function(){
     Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.categories.add');
     Route::get('/admin/category/edit/{category_id}',AdminEditCategoryComponent::class)->name('admin.category.edit');
     Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
+    Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.product.add');
 });
 
 // Route::get('/dashboard', function () {
