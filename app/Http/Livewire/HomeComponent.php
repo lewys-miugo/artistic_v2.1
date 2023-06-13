d62514b14320adb25cd697e93e3bf9cc258230af
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-13 19:23:41 +0300

    Final touches

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -4,13 +4,24 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use App\Models\Category;
+use Cart;
 
 class HomeComponent extends Component
 {
+    public function store($product_id,$product_name,$product_price)
+    {
+        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
+        session()->flash('success_message','Item added in Cart');
+        $this->emitTo('cart-icon-component','refreshComponent');
+        return redirect()->route('shop.cart');
+    }
+
     public function render()
     {
-        $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(8);
-        $fproducts = Product::where('featured',1)->inRandomOrder()->get()->take(8);
-        return view('livewire.home-component',['lproducts'=>$lproducts,'fproducts'=>$fproducts]);
+        $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(16);
+        $fproducts = Product::where('featured',1)->inRandomOrder()->get()->take(16);
+        $categories = Category::orderBy('name','ASC')->get();
+        return view('livewire.home-component',['lproducts'=>$lproducts,'fproducts'=>$fproducts,'categories'=>$categories]);
     }
 }
