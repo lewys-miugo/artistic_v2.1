23be257f899412f53920d08eeb3facef783db7a1
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-16 12:23:03 +0300

    adding password constraints

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->mixedCase()->symbols()->numbers();
             $table->string('utype')->default('USR')->comment('ADM for admin and USR for user');
             $table->rememberToken();
             $table->timestamps();
