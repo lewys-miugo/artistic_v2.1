644f9cdfa9a06345a2c0a6fa71dd8a63e4f19725
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-16 11:24:59 +0300

    Messing around

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,6 +32,7 @@ class UserFactory extends Factory
      * @return static
      */
     public function unverified()
+
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
