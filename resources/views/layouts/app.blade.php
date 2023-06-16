b87e4d35aae088de7e3f9f07b656c8957247765f
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-16 11:29:24 +0300

    Adding favicon and app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> -->
+        <link rel="icon" href="{{asset('ae_w_favicon.png')}}">
+        <title>{{-- config('app.name', 'Artistic Excursions') --}}Artistic Excursions</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
