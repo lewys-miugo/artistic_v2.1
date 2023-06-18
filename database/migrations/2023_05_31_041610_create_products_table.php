2df794ff37d3841c9640e8776e66e4d17b712851
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-18 13:45:34 +0300

    Making image tag nullable

--- database/migrations/2023_05_31_041610_create_products_table.php
+++ database/migrations/2023_05_31_041610_create_products_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->enum('stock_status',['instock','outofstock']);
             $table->boolean('featured')->default(false);
             $table->unsignedInteger('quantity')->default(10);
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->text('images')->nullable();
             $table->bigInteger('category_id')->unsigned()->nullable();
             $table->timestamps();
