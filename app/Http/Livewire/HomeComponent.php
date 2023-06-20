161d846e4c74c0db0a13537748965e2bf9fc3bcf
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-20 12:00:45 +0300

    Adding wishlist

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -17,11 +17,32 @@ class HomeComponent extends Component
         return redirect()->route('shop.cart');
     }
 
+    public function addToWishlist($product_id,$product_name,$product_price)
+    {
+        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        $this->emitTo('wishlist-icon-component','refreshComponent');
+    }
+
+    public function removeFromWishlist($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            if($witem->id==$product_id)
+            {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-icon-component','refreshComponent');
+                return;
+            }
+        }
+    }
+
     public function render()
     {
         $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(12);
         $fproducts = Product::where('featured',1)->inRandomOrder()->get()->take(16);
         $categories = Category::orderBy('name','ASC')->get()->take(5);
-        return view('livewire.home-component',['lproducts'=>$lproducts,'fproducts'=>$fproducts,'categories'=>$categories]);
+        $products = Product::paginate(12);            
+
+        return view('livewire.home-component',['lproducts'=>$lproducts,'products'=>$products,'fproducts'=>$fproducts,'categories'=>$categories]);
     }
 }
