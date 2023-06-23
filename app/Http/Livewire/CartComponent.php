30021849f5a16a058934b9aa6e300eb14d19bb58
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-23 22:12:54 +0300

    adding Checkout function

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,17 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 use Cart;
 
 class CartComponent extends Component
 
 {
-    public $cartTotal;
+    // public $cartTotal;
 
-    public function mount()
-    {
-        $this->cartTotal=Cart::total();
-    }
+    // public function mount()
+    // {
+    //     $this->cartTotal=Cart::total();
+    // }
 
 
     public function increaseQuantity($rowId)
@@ -45,9 +46,41 @@ class CartComponent extends Component
         session()->flash('success_message','Cart has been cleared');
     }
 
+    public function checkout()
+    {
+        if (Auth::check()) {
+            return redirect()->route('checkout');
+            return redirect('/checkout');
+
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
+        // Somewhere in your code when initiating the checkout process
+        // session()->put('checkout', true);
+
+        // return redirect()->route('checkout');
+
+    }
+
+    public function setAmountForCheckout()
+    {
+        if (!Cart::instance('cart')->count()>0) {
+            session()->forget('checkout');
+            return;
+        }
+        session()->put('checkout',[
+            'subtotal'=>Cart::instance('cart')->subtotal(),
+            'delivery'=>Cart::instance('cart')->tax(),
+            'total'=>Cart::instance('cart')->total()
+        ]);
+    }
 
     public function render()
     {
+
+        $this->setAmountForCheckout();
         return view('livewire.cart-component');
     }
 }
