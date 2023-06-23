76d8947b22565356208724b505a49425b08ec761
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-23 22:09:55 +0300

    Thank you page for making orders

---
+++ resources/views/livewire/thankyou-component.blade.php
@@ -0,0 +1,25 @@
+<div>
+    {{-- Do your work, then step back. --}}
+    <div class="m-4">
+        <div>
+                <p class="text-left text-xs uppercase text-[#6a767e]"><a href="/" class="uppercase">Home</a> > Thankyou</p>
+        </div>
+    </div>
+    <div class="flex flex-col items-center justify-center min-h-screen">
+        <div class="bg-white p-8 rounded-lg shadow-lg">
+            <h1 class="text-3xl font-semibold text-center mb-4">Thank You!</h1>
+            <div class="bg-blue-500 text-white px-6 py-4 rounded-lg mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                </svg>
+                <p class="text-lg text-center">Your order has been successfully placed.</p>
+            </div>
+            <p class="text-gray-700 text-center mb-6">Order Number: #123456</p>
+            <div class="text-center">
+                <p class="text-gray-700 mb-2">We will send you a confirmation email shortly.</p>
+                <p class="text-gray-700">If you have any questions, please contact our support team.</p>
+            </div>
+            <a href="/" class="mt-8 inline-block text-blue-500 underline hover:text-blue-700">Back to Home</a>
+        </div>
+    </div>
+</div>
