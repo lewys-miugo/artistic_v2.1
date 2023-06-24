64bbfa693ab4ff41d24e4523a52ab8044f6ef2c8
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-24 16:59:57 +0300

    Creating order page functionality

---
+++ app/Http/Livewire/User/UserOrderComponent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Order;
+use Livewire\WithPagination;
+
+
+class UserOrderComponent extends Component
+{
+    use WithPagination;
+    public function render()
+    {
+        $orders=Order::where('user_id',Auth::user()->id)->paginate(12);
+        return view('livewire.user.user-order-component',['orders'=>$orders]);
+    }
+}
