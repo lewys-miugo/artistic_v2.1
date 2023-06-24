2eb2c28c3ae6812229bd6bf3acb71e037c6d9985
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-24 19:05:07 +0300

    Adding the action button for delivered and cancel order

--- resources/views/livewire/admin/admin-order-component.blade.php
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -1,8 +1,12 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
-    <div class="bg-gray-100">
+    <div class="bg-gray-100 mx-4">
         <div>All Orders</div>
-        <table class="w-full">
+        @if(Session::has('order_message'))
+            <div>{{Session::get('order_message')}}</div>
+        @endif
+        <div class="overflow-x-auto">
+            <table class="w-full">
             <thead>
                 <tr>
                     <th class="py-2 px-4 bg-blue-500 text-white">OrderID</th>
@@ -17,6 +21,9 @@
                     <th class="py-2 px-4 bg-blue-500 text-white">Status</th>
                     <th class="py-2 px-4 bg-blue-500 text-white">Order Date</th>
                     <th class="py-2 px-4 bg-blue-500 text-white">Action</th>
+                    <th class="py-2 px-4 bg-blue-500 text-white">status</th>
+
+
                 </tr>
             </thead>
             <tbody>
@@ -34,10 +41,17 @@
                         <td class="py-2 px-4">{{$order->status}}</td>
                         <td class="py-2 px-4">{{$order->created_at}}</td>
                         <td class="py-2 px-4"><a href="{{route('admin.orderdetails',['order_id'=>$order->id])}}">details</a></td>
+                        <td class="py-2 px-4">
+                            <ul>
+                                <li><a href="#" wire:click.prevent="updateOrderStatus({{$order->id}},'delivered')">Delivered</a></li>
+                                <li><a href="#" wire:click.prevent="updateOrderStatus({{$order->id}},'canceled')">Canceled</a></li>
+                            </ul>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+        </div>
         {{$orders->links()}}
     </div>
 </div>
