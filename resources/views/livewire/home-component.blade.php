5d44133c62e2e8d77322ae139896d916496f6481
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-25 19:29:04 +0300

    Adding mutiple images

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -384,44 +384,57 @@
                 @foreach($lproducts as $lproduct)
 
 
-                <div class="flex flex-col m-2">
-                    <a href="{{route('product.details',['slug'=>$lproduct->slug])}}">
-                        <img src="{{asset('images/products')}}/{{$lproduct->image}}" alt="{{$lproduct->name}}" class="h-64">
+                    <div class="flex flex-col m-2">
+                        <a href="{{route('product.details',['slug'=>$lproduct->slug])}}">
+                            <!-- <img src="{{asset('images/products')}}/{{$lproduct->image}}" alt="{{$lproduct->name}}" class="h-64"> -->
+                            @php
+                                $images = App\Models\ProductImages::where('product_unique_id',$lproduct->unique_id)->get();
+                            @endphp
+                                <div class="swiper mySwiper w-64 h-64">
+                                    <div class="swiper-wrapper ">
+                                        @foreach($images as $item)
+                                            <div class="swiper-slide flex justify-center content-center m-auto"> 
+                                                <img src="{{ asset('uploads/all')}}/{{$item->image}}" class="w-full h-auto" alt="">
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                    <div class="swiper-pagination"></div>
+                                </div>
 
 
-                        <ul class="flex flex-row text-[#46555f] text-base mt-2 pl-2">
-                            <li class="text-xs">{{$lproduct->name}}</li>
-                            
-                        </ul>
+                            <ul class="flex flex-row text-[#46555f] text-base mt-2 pl-2">
+                                <li class="text-xs">{{$lproduct->name}}</li>
+                                
+                            </ul>
 
-                        <ul class="flex flex-row text-[#46555f] text-base pl-2 justify-end">
-                            <li class=" flex flex-row justify-end">
-                                @if($witems->contains($lproduct->id))
-                                    <a href="#" wire:click.prevent="removeFromWishlist({{$lproduct->id}})">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
-                                            <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-                                        </svg>
-                                    </a>
-                                @else
-                                    <a href="" wire:click.prevent="addToWishlist({{$lproduct->id}},'{{$lproduct->name}}',{{$lproduct->regular_price}})">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                        </svg>
-                                    </a>
-                                @endif
-                                <!-- Cart -->
-                                    <a href="#" wire:click.prevent="store({{$lproduct->id}},'{{$lproduct->name}}',{{$lproduct->regular_price}})">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-4 ">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                        </svg>
-                                    </a>
-                            </li>
-                        </ul>
+                            <ul class="flex flex-row text-[#46555f] text-base pl-2 justify-end">
+                                <li class=" flex flex-row justify-end">
+                                    @if($witems->contains($lproduct->id))
+                                        <a href="#" wire:click.prevent="removeFromWishlist({{$lproduct->id}})">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                                            </svg>
+                                        </a>
+                                    @else
+                                        <a href="" wire:click.prevent="addToWishlist({{$lproduct->id}},'{{$lproduct->name}}',{{$lproduct->regular_price}})">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                            </svg>
+                                        </a>
+                                    @endif
+                                    <!-- Cart -->
+                                        <a href="#" wire:click.prevent="store({{$lproduct->id}},'{{$lproduct->name}}',{{$lproduct->regular_price}})">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-4 ">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                                            </svg>
+                                        </a>
+                                </li>
+                            </ul>
 
-                        <p class="text-xs pl-2 text-[#46555f] italic">{{$lproduct->regular_price}}</p>
-                    </a>
+                            <p class="text-xs pl-2 text-[#46555f] italic">{{$lproduct->regular_price}}</p>
+                        </a>
 
-                </div>
+                    </div>
                 @endforeach
             </div>
         </div>
@@ -462,7 +475,20 @@
 
                 <div class="flex flex-col m-2">
                     <a href="{{route('product.details',['slug'=>$fproduct->slug])}}">
-                        <img src="{{asset('images/products')}}/{{$fproduct->image}}" alt="{{$fproduct->name}}" class="h-64">
+                        <!-- <img src="{{asset('images/products')}}/{{$fproduct->image}}" alt="{{$fproduct->name}}" class="h-64"> -->
+                        @php
+                                        $images = App\Models\ProductImages::where('product_unique_id',$fproduct->unique_id)->get();
+                                    @endphp
+                                    <div class="swiper mySwiper w-64 h-64">
+                                        <div class="swiper-wrapper ">
+                                            @foreach($images as $item)
+                                                <div class="swiper-slide flex justify-center content-center m-auto"> 
+                                                    <img src="{{ asset('uploads/all')}}/{{$item->image}}" class="w-full h-auto" alt="">
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                        <div class="swiper-pagination"></div>
+                                    </div>
 
 
                         <ul class="flex flex-row text-[#46555f] text-base mt-2 pl-2">
