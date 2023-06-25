2c25015e6243d5526c28971f6b7d64216fba8c1b
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-25 19:21:10 +0300

    display multiple images

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -5,18 +5,31 @@ namespace App\Http\Livewire\Admin;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Product;
+use App\Models\ProductImages;
+
 use App\Models\Category;
 
 class AdminProductComponent extends Component
 {
     use WithPagination;
     public $product_id;
+    
 
     public function deleteProduct()
     {
         $product = Product::find($this->product_id);
-        unlink('images/products/'.$product->image);
+        // unlink('images/products/'.$product->image);
+        // $image->delete();
         $product->delete();
+
+
+        // multiple images
+        $image=ProductImages::where('product_id')->get();
+        foreach($images as $image){
+            $image->delete();
+        }
+
+        
         session()->flash('message','Product has been deleted!!');
     }
     
