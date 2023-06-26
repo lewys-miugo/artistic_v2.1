861ae6cca91aa5ea2f4a13296bb50444581ac0a1
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-26 21:38:02 +0300

    Adding f prices and removing some unecassary data

--- database/migrations/2023_05_31_041610_create_products_table.php
+++ database/migrations/2023_05_31_041610_create_products_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->decimal('regular_price',8,2)->nullable();
             $table->decimal('regular_price_A0',8,2)->default('5000.00')->nullable();
+            $table->decimal('regular_price_A05',8,2)->default('5000.00')->nullable();
             $table->decimal('regular_price_A1',8,2)->default('4500.00')->nullable();
             $table->decimal('regular_price_A2',8,2)->default('3000.00')->nullable();
             $table->decimal('regular_price_A3',8,2)->default('2500.00')->nullable();
@@ -34,13 +35,25 @@ return new class extends Migration
             $table->decimal('regular_price_4p',8,2)->default('8000.00')->nullable();
             $table->decimal('regular_price_5p',8,2)->default('9000.00')->nullable();
             $table->decimal('regular_price_pa',8,2)->default('10000.00')->nullable();
+            // Framed paper
+            $table->decimal('regular_price_A0f',8,2)->default('5000.00')->nullable();
+            $table->decimal('regular_price_A05f',8,2)->default('5000.00')->nullable();
+            $table->decimal('regular_price_A1f',8,2)->default('4500.00')->nullable();
+            $table->decimal('regular_price_A2f',8,2)->default('3000.00')->nullable();
+            $table->decimal('regular_price_A3f',8,2)->default('2500.00')->nullable();
+            $table->decimal('regular_price_A4f',8,2)->default('2000.00')->nullable();
+            $table->decimal('regular_price_2pf',8,2)->default('6000.00')->nullable();
+            $table->decimal('regular_price_3pf',8,2)->default('7000.00')->nullable();
+            $table->decimal('regular_price_4pf',8,2)->default('8000.00')->nullable();
+            $table->decimal('regular_price_5pf',8,2)->default('9000.00')->nullable();
+            $table->decimal('regular_price_paf',8,2)->default('10000.00')->nullable();
 
 
-            $table->decimal('sale_price',8,2)->nullable();
-            $table->string('SKU')->nullable();
-            $table->enum('stock_status',['instock','outofstock']);
+            // $table->decimal('sale_price',8,2)->nullable();
+            // $table->string('SKU')->nullable();
+            // $table->enum('stock_status',['instock','outofstock']);
             $table->boolean('featured')->default(false);
-            $table->unsignedInteger('quantity')->default(10);
+            // $table->unsignedInteger('quantity')->default(10);
             $table->string('image')->nullable();
             $table->text('images')->nullable(); 
             $table->bigInteger('category_id')->unsigned()->nullable();
