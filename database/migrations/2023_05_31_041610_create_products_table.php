98590a91f40f2e45c83d8052f505b644a8df0bab
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-27 18:44:21 +0300

    Making images unsigned()

--- database/migrations/2023_05_31_041610_create_products_table.php
+++ database/migrations/2023_05_31_041610_create_products_table.php
@@ -55,7 +55,7 @@ return new class extends Migration
             $table->boolean('featured')->default(false);
             // $table->unsignedInteger('quantity')->default(10);
             $table->string('image')->nullable();
-            $table->text('images')->nullable(); 
+            $table->text('images')->unsigned()->nullable(); 
             $table->bigInteger('category_id')->unsigned()->nullable();
             $table->timestamps();
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
