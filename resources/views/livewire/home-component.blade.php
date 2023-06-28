d7f66dfa6308c84cca9e8bd2af54be3cdc5cf6a7
lewys-miugo <lewiswambugu01@gmail.com>
2023-06-28 14:17:45 +0300

    Making the categories centered

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,13 +1,13 @@
 <div>
     <div class="">
         <ul class="overflow-x-auto gray-300 list-none flex flex-row  pl-2 mx-2 text-[#46555f]">
-            <li class=" grow border-y-2 transition ease-in-out	delay-200 duration-100 hover:border-t-[#c45472]">
+            <li class=" grow border-y-2 transition ease-in-out text-center justify-center	delay-200 duration-100 hover:border-t-[#c45472]">
                 <a href="{{route('shop')}}" class="mx-2 my-2 uppercase text-sm font-bold hover:text-[#273137]">
                     All
                 </a>
             </li>
             @foreach($categories as $category)
-                <li class="grow border-y-2  transition ease-in-out	delay-200 duration-100 hover:border-t-[#c45472]"><a href="{{route('product.category',['slug'=>$category->slug])}}" class="mx-2 my-2 uppercase text-sm font-bold hover:text-[#273137]">{{$category->name}}</a></li>
+                <li class="grow border-y-2  transition ease-in-out text-center justify-center	delay-200 duration-100 hover:border-t-[#c45472]"><a href="{{route('product.category',['slug'=>$category->slug])}}" class="mx-2 my-2 uppercase text-sm font-bold hover:text-[#273137]">{{$category->name}}</a></li>
 
             @endforeach
         </ul>
