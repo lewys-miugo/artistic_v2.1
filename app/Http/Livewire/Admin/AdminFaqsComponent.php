9a5244cf57e7efb23148628a99fd6bdee46225e2
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-06 02:31:45 +0300

    Creating the adminfaqscomponent

---
+++ app/Http/Livewire/Admin/AdminFaqsComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminFaqsComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-faqs-component');
+    }
+}
