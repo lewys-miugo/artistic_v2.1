5112168bc04ad714df2f6b955e49d7d3eb7b6d20
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-06 02:33:04 +0300

    creating the admineditfaqcomponent

---
+++ app/Http/Livewire/Admin/AdminEditFaqComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminEditFaqComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-edit-faq-component');
+    }
+}
