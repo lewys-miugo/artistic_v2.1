d9725e2718b8b07e4bfbc1d54901cf0f1eea724c
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-06 12:47:48 +0300

    delete functionality

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -13,21 +13,66 @@ class AdminProductComponent extends Component
 {
     use WithPagination;
     public $product_id;
+
+    // public $image;
+    // public $image2;
+    // public $image3;
+    // public $image4;
+    // public $image5;
+    // public $image6;
+    // public $image7;
     
 
     public function deleteProduct()
     {
         $product = Product::find($this->product_id);
-        // unlink('images/products/'.$product->image);
-        // $image->delete();
+
+        if($product->image)
+        {
+            unlink('images/products/'.$product->image);
+            // $image->delete();
+        }
+        if($product->image2)
+        {
+            unlink('images/products/'.$product->image2);
+            // $image2->delete();
+        }
+
+        if($product->image3)
+        {
+            unlink('images/products/'.$product->image3);
+            // $image3->delete();
+        }
+
+        if($product->image4)
+        {
+            unlink('images/products/'.$product->image4);
+            // $image4->delete();
+        }
+
+        if($product->image5)
+        {
+            unlink('images/products/'.$product->image5);
+            // $image5->delete();
+        }
+
+        if($product->image6)
+        {
+            unlink('images/products/'.$product->image6);
+            // $image6->delete();
+        }
+
+        if($product->image7)
+        {
+            unlink('images/products/'.$product->image7);
+            // $image7->delete();
+        }
+
         $product->delete();
 
 
         // multiple images
-        $image=ProductImages::where('product_id')->get();
-        foreach($images as $image){
-            $image->delete();
-        }
+        
 
         
         session()->flash('message','Product has been deleted!!');
