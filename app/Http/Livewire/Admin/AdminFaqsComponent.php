05eff1553c6547cbd4fcd4c9aaaa5bf211022e3e
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-06 04:03:17 +0300

    Adding functionality to delete an faq

--- app/Http/Livewire/Admin/AdminFaqsComponent.php
+++ app/Http/Livewire/Admin/AdminFaqsComponent.php
@@ -3,11 +3,27 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Faq;
+use Livewire\WithPagination;
+
 
 class AdminFaqsComponent extends Component
 {
+    use WithPagination;
+
+    public $faq_id;
+
+    public function deleteFaq()
+    {
+        $faq= Faq::find($this->faq_id);
+        $faq->delete();
+        session()->flash('message','Faq has been deleted succesfully');
+    }
+
     public function render()
     {
-        return view('livewire.admin.admin-faqs-component');
+
+        $faqs=Faq::orderBy('question','ASC')->paginate(10);
+        return view('livewire.admin.admin-faqs-component',['faqs'=>$faqs]);
     }
 }
