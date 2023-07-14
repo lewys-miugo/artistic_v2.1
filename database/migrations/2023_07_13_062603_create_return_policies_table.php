3953faaf9b8ca6d784da27a9333306f021595b09
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-15 01:26:23 +0300

    Return policies table

---
+++ database/migrations/2023_07_13_062603_create_return_policies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('return_policies', function (Blueprint $table) {
+            $table->id();
+            $table->text('return_policy')->nullable()->default('return policy 101');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('return_policies');
+    }
+};
