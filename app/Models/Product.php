5b23a96c20e055581ee43846d6d5ddb76a55a34c
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-21 13:30:28 +0300

    adding theme option

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,4 +17,9 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class,'category_id');
     }
+
+    public function theme()
+    {
+        return $this->belongsTo(Theme::class,'theme_id');
+    }
 }
