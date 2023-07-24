8fedb2a3693c7ccc85aa37b1e9b1a3779ba94de2
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-24 09:07:13 +0300

    Remoing theme row

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -19,25 +19,25 @@
         </ul>
     </div>
 
-    <div class="theme-scroll">
+    <!-- <div class="theme-scroll">
         <ul class="overflow-x-auto gray-300 list-none flex flex-row  pl-2 mx-2 text-[#46555f]">
             <li class=" grow border-y-2 transition ease-in-out text-center justify-center	delay-200 duration-100 hover:border-t-[#c45472]">
                 <a href="{{-- route('theme') --}}" class="mx-2 my-2 uppercase text-sm font-bold hover:text-[#273137]">
                   All
                 </a>
             </li>
-            <!-- <li class=" grow border-y-2 transition ease-in-out text-center justify-center	delay-200 duration-100 hover:border-t-[#c45472]">
+            <li class=" grow border-y-2 transition ease-in-out text-center justify-center	delay-200 duration-100 hover:border-t-[#c45472]">
                 <a href="{{route('shop')}}" class="mx-2 my-2 uppercase text-sm font-bold hover:text-[#273137]">
                   All-test
                 </a>
-            </li> -->
+            </li>
             @foreach($themes as $theme)
                 <li class="grow border-y-2  transition ease-in-out text-center justify-center	delay-200 duration-100 hover:border-t-[#c45472]">
-                    <a href="{{route('product.theme',['slug'=>$theme->slug])}}" class="mx-2 my-2 uppercase text-sm font-bold hover:text-[#273137]">{{$theme->name}}</a>
+                    <a href="{{-- route('product.theme',['slug'=>$theme->slug]) --}}" class="mx-2 my-2 uppercase text-sm font-bold hover:text-[#273137]">{{-- $theme->name --}}</a>
                 </li>
             @endforeach
         </ul>
-    </div>
+    </div> -->
 
 <div class="mx-4 mb-4">
     <!-- 3-banner-images -->
