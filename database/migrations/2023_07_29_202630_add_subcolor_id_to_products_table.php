bbe5fd6cf8218a84f9851a434da26549d8e6dcb4
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-30 18:14:34 +0300

    Migration to add subcoor_id to products table

---
+++ database/migrations/2023_07_29_202630_add_subcolor_id_to_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->bigInteger('subcolor_id')->unsigned()->nullable();
+            $table->foreign('subcolor_id')->references('id')->on('sub_colors')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+            $table->dropForeign('products_subcolor_id_foreign');
+            $table->dropColumn('subcolor_id');
+        });
+    }
+};
