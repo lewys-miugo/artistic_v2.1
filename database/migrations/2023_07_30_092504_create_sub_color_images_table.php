35c507a6db792337ad5e7ac6569c4d09822ce963
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-30 18:15:52 +0300

    Sub Color Image

---
+++ database/migrations/2023_07_30_092504_create_sub_color_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_color_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('sub_color_unique_id')->nullable();
+            $table->string('sub_color_id')->nullable();
+            $table->string('image')->nullable();
+            $table->string('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_color_images');
+    }
+};
