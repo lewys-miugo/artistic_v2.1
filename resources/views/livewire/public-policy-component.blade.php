67d4baa283761a43379d77ebcbf426c1a6d34cb7
lewys-miugo <lewiswambugu01@gmail.com>
2023-08-22 06:55:16 +0300

    Privacy policy component

---
+++ resources/views/livewire/public-policy-component.blade.php
@@ -0,0 +1,119 @@
+<div class="m-8">
+    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <div class="space-y-4">
+        <h2 class="text-center">Privacy Policy for Artistic Excursions</h2>
+        <h4>Last Update: 22nd August 2023</h4>
+        <!-- 1 -->
+        <div>
+            <h2 class="text-xl font-bold">1. Introduction</h2>
+            <p class="ml-2">
+                Welcome to Artistic Excursions. We are committed to protecting your privacy and safeguarding your personal information. This Privacy Policy outlines how we collect, use, disclose, and protect the personal information you provide while using our services.
+            </p>
+        </div>
+        <!-- 2 -->
+        <div>
+            <h2 class="text-xl font-bold">2. Information We Collect</h2>
+            <div class="space-y-2 ml-2">
+                <p >
+                    <strong>Personal Information: </strong> We may collect personal information such as your name, email address, shipping information, phone number, and payment information when you make a purchase.On signing up or interact with our site's features we will only collect your name and email.
+                </p>  
+                <p>
+                    <strong> Usage Information: </strong>
+                    We collect information about how you use our site, including your browsing activity, the pages you visit, and the products you view.
+                </p>  
+                <p>
+                    <strong> Cookies and Similar Technologies: </strong> We use cookies and similar technologies to enhance your experience on our site. These technologies may collect information about your device, browser, and preferences.
+                </p>
+            </div>
+        </div>
+
+        <!-- 3 -->
+        <div>
+            <h2 class="text-xl font-bold">3. How We Use Your Information</h2>
+            <div class="ml-2">
+                <p>
+                    <strong> Provide and Improve Services: </strong> We use your information to process orders, provide customer support, and enhance your experience on our site.
+                </p>
+                <p>
+                    <strong> Communication: </strong> We may use your contact information to send you transactional emails only. 
+                    
+                    <!-- updates, and marketing communications. You can opt-out of marketing communications at any time. -->
+                </p>
+                <p>
+                    <strong>Analytics:</strong> We analyze usage data to improve our site's functionality and user experience.
+                </p>
+            </div>
+        </div>
+
+        <!-- 4 -->
+        <div>
+            <h2 class="text-xl font-bold">4. Sharing Your Information</h2>
+            <div class="ml-2">
+                <p>
+                    <strong> Service Providers:  </strong> We may share your information with trusted third-party service providers who assist us in operating our business, such as payment processors, shipping partners, and IT service providers.
+                </p>
+                <p>
+                    <strong> Legal Compliance: </strong> We may disclose your information if required by law, to protect our rights, or to comply with legal processes.
+                </p>
+            </div>
+        </div>
+
+        <!-- 5 -->
+        <div>
+            <h2 class="text-xl font-bold">5. Your Choices</h2>
+            <div class="ml-2">
+                <p>
+                    <strong> Account Settings:  </strong> You can review and update your account information by logging into your account.
+                </p>
+                <p>
+                    <strong> Cookies: </strong> Most web browsers allow you to control cookies through their settings.
+                </p>
+            </div>
+        </div>
+
+        <!-- 6 -->
+        <div>
+            <h2 class="text-xl font-bold">6. Security</h2>
+            <div class="ml-2">
+                <p>
+                    <!-- <strong> Account Settings:  </strong>  -->
+                    We employ industry-standard security measures to protect your personal information. However, please note that no data transmission over the internet or storage method is 100% secure.
+                </p>
+            </div>
+        </div>
+
+        <!-- 7 -->
+        <div>
+            <h2 class="text-xl font-bold">7. Children's Policy</h2>
+            <div class="ml-2">
+                <p>
+                    <!-- <strong> Account Settings:  </strong>  -->
+                    We do not knowingly collect personal information from children.
+                </p>
+            </div>
+        </div>
+
+        <!-- 8 -->
+        <div>
+            <h2 class="text-xl font-bold">8. Changes to this Privacy Poliicy</h2>
+            <div class="ml-2">
+                <p>
+                    <!-- <strong> Account Settings:  </strong>  -->
+                    We may update this Privacy Policy to reflect changes in our practices. We will notify you of any significant changes through our site or via email.
+                </p>
+            </div>
+        </div>
+
+        <!-- 9 -->
+        <div>
+            <h2 class="text-xl font-bold">9. Contact Us</h2>
+            <div class="ml-2">
+                <p>
+                    <!-- <strong> Account Settings:  </strong>  -->
+                    If you have any questions or concerns about our Privacy Policy or the way we handle your information, please contact us at artisticexcursions@gmail.com .
+                </p>
+            </div>
+        </div>
+        
+    </div>
+</div>
