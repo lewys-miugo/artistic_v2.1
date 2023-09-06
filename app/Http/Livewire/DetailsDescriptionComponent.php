b00595b9acf1722acf7258b9f508d3854e17faab
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-06 22:09:13 +0300

    building host for the AR buttons

---
+++ app/Http/Livewire/DetailsDescriptionComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DetailsDescriptionComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.details-description-component');
+    }
+}
