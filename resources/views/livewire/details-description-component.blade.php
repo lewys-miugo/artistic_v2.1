5144b99d3163489536cb7d640595de09ebbcdb78
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-06 22:07:53 +0300

    Building the details to hold AR in place

---
+++ resources/views/livewire/details-description-component.blade.php
@@ -0,0 +1,15 @@
+<div class="mt-8">
+    <h2 class="text-2xl font-bold mb-4">Art Pieces Details</h2>
+    <ul class="list-disc list-inside">
+        <p class="text-gray-600 mb-6">The Canvas Prices are for artworks that are frameless, the floating frames are artworks whose frame has a slight margin between the artwork and the frame. see below</p>
+        <p class="text-gray-600 mb-6"> For the sizes:
+            <ul class="space-y-2">
+                <li>A0=84.1cm x 118.9cm <span><a href="{{route('A0.ar')}}" class=" ml-2 inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-700">A0 AR view</a></span></li>
+                <li>A1=59.4cm x  84.1cm <span><a href="{{route('A1.ar')}}" class="ml-3  inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-700">A1 AR view</a></span></li>
+                <li>A2=42.0cm x  59.4cm <span><a href="{{route('A2.ar')}}" class="ml-2 inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-700">A2 AR view</a></span></li>
+                <li>A3=29.7cm x  42.0cm <span><a href="{{route('A3.ar')}}" class="ml-2 inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-700">A3 AR view</a></span></li>
+                <li>A4=21cm x 29.7cm <span><a href="{{route('A4.ar')}}" class="ml-4 inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-700">A4 AR view</a></span></li>
+            </ul>
+        </p>
+    </ul>
+</div>
