b6e92d10ad715c5ea12fd4979f496a34d74a71f3
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-18 20:40:09 +0300

    Adding color/subcolor

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -4,11 +4,17 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use App\Models\Color;
+use App\Models\SubColor;
+
+// use App\Models\User;
 use Cart;
 
 class DetailsComponent extends Component
 {
     public $slug;
+    public $color;
+    public $subcolor;
 
     public function mount($slug)
     {
@@ -27,6 +33,8 @@ class DetailsComponent extends Component
         $product = Product::where('slug',$this->slug)->first();
         $rproducts = Product::where('category_id',$product->category_id)->inRandomOrder()->limit(4)->get();
         $nproducts = Product::latest()->take(4)->get(); 
+        // $color = Color::where('color_id',$product->color_id)->get();
+        // ,'color'=>$color
         return view('livewire.details-component',['product'=>$product,'rproducts'=>$rproducts,'nproducts'=>$nproducts]);
     }
 }
