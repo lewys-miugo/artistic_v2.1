567c0bb6f2ca499a4c40d9abb9a3c3cd217f979c
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-20 15:17:57 +0300

    Site under maintenance page development

---
+++ app/Http/Livewire/SiteUnderMaintenanceComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SiteUnderMaintenanceComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.site-under-maintenance-component');
+    }
+}
---
+++ resources/views/livewire/site-under-maintenance-component.blade.php
@@ -0,0 +1,21 @@
+<div class="bg-gray-100">
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="text-center">
+            <!-- Animated icon (e.g., spinning cog) -->
+            <div class="animate-spin text-blue-500 text-6xl mb-4">
+                <!-- Use a suitable Heroicon for a maintenance icon -->
+                <svg class="w-16 h-16 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 6V6m0 6h6m-6 0H6"></path>
+                </svg>
+            </div>
+            <h1 class="text-3xl font-semibold text-gray-800 mb-2">Sorry, this page is</h1>
+            <p class="text-xl text-gray-600">under maintenance. Check back soon!</p>
+            <p class="text-xl text-gray-600">Click 
+            <a href="{{route('shop')}}" class="text-blue-300 underline">here</a>
+                
+            to continue exploring.
+
+            </p>
+        </div>
+    </div>
+</div>
