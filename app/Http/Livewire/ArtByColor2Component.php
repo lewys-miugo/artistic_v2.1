516f0eef01ff334b4e9166a5d22a9e44b9da152e
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-25 06:42:38 +0300

    testing one 2 one 2

---
+++ app/Http/Livewire/ArtByColor2Component.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Color;
+use App\Models\SubColor;
+use Livewire\WithPagination;
+use Cart;
+
+class ArtByColor2Component extends Component
+{
+
+    use WithPagination;
+    public $pageSize=18;
+    public $color_slug;
+    public $orderBy= "Default";
+
+    public $scolor_slug;
+
+    public function changeOrderBy($order)
+    {
+        $this->orderBy = $order;
+    }
+
+    public function mount($slug,$scolor_slug=null)
+    {
+        $this->slug=$slug;
+        $this->scolor_slug =$scolor_slug;
+    }
+
+    public function addToWishlist($product_id,$product_name,$product_price)
+    {
+        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        $this->emitTo('wishlist-icon-component','refreshComponent');
+    }
+
+    public function removeFromWishlist($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            if($witem->id==$product_id)
+            {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-icon-component','refreshComponent');
+                return;
+            }
+        }
+    }
+
+
+    public function render()
+    {
+        $color_id=null;
+        $color_name="";
+        // $color=;
+
+        if ($this->scolor_slug) {
+            $scolor = SubColor::where('slug',$this->scolor_slug)->first();
+            // $color->$scolor;
+            $color_id=$scolor->id;
+            $color_name=$scolor->name;
+            
+        }
+        
+        else{
+            $color=Color::where('slug',$this->slug)->first();
+            $color_id=$color->id;
+            $color_name=$color->name;
+        }
+
+        // $color=Color::where('slug',$this->slug)->first();
+        // $color_id=$color->id;
+        // $color_name=$color->name;
+
+        if($this->orderBy == 'Latest')
+        {
+            $products = Product::where('color_id',$color_id)->orderBy('created_at','DESC')->paginate(18);
+
+        }
+        else{
+            $products = Product::where('color_id',$color_id)->paginate(18);            
+        }
+
+        $colors = Color::orderBy('name','ASC')->get();
+        $color = Color::where('slug',$this->slug)->first();
+
+        $categories=Category::orderBy('name','ASC')->get();
+
+        return view('livewire.art-by-color2-component',['products'=>$products,'categories'=>$categories, 'colors'=>$colors,'color_name'=>$color_name,'color'=>$color]);
+    }
+}
