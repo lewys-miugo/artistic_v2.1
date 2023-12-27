691488f8e6e6882143924516b9fbe6d88d3a58f4
lewys-miugo <lewiswambugu01@gmail.com>
2023-12-27 10:44:22 +0300

    Art gallery

---
+++ app/Http/Livewire/ArtGalleryComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ArtGalleryComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.art-gallery-component');
+    }
+}
